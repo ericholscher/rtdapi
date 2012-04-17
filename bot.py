62ed2e9f85ef6a54c3eb52c269e90860e23940d2
Eric Holscher <eric@ericholscher.com>
2012-04-17 11:26:48 -0700

    Lots o' updates.

--- bot.py
+++ bot.py
@@ -3,6 +3,10 @@ import redis
 import slumber
 import string
 import shlex
+import re
+from dateutil import parser
+from datetime import datetime
+
 
 host = "localhost"
 r = redis.Redis(host=host)
@@ -30,7 +34,44 @@ def output():
                 if command in data['message']:
                     output = func(data)
                     if output:
-                        send(data['channel'], output)
+                        if data['channel']:
+                            print "Sending to channel"
+                            send(data['channel'], output)
+                        else:
+                            print "Sending to sender"
+                            send(data['sender'], output)
+
+
+
+def timesince(dt, default="just now"):
+    """
+    Returns string representing "time since" e.g.
+    3 days ago, 5 hours ago etc.
+    """
+
+    now = datetime.utcnow()
+    diff = now - dt
+    
+    periods = (
+        (diff.days / 365, "year", "years"),
+        (diff.days / 30, "month", "months"),
+        (diff.days / 7, "week", "weeks"),
+        (diff.days, "day", "days"),
+        (diff.seconds / 3600, "hour", "hours"),
+        (diff.seconds / 60, "minute", "minutes"),
+        (diff.seconds, "second", "seconds"),
+    )
+
+    for period, singular, plural in periods:
+        
+        if period:
+            return "%d %s ago" % (period, singular if period == 1 else plural)
+
+    return default
+
+##########
+########## COMMANDS
+##########
 
 def Info(data):
     import inspect
@@ -61,12 +102,33 @@ def Info(data):
 def Status(data):
     cmd = '!%s ' % string.lower(Status.__name__)
     project = data['message'].replace(cmd, '')
-    val = api.build(project).get(limit=1)
-    #import ipdb; ipdb.set_trace()
-    obj = val['objects'][0]
-    status = "Passed" if obj['success'] else "Failed"
-    ret_val = "%s: http://readthedocs.org%s (%s)" % (status, obj['absolute_url'], obj['date'])
-    return ret_val
+    try:
+        val = api.build(project).get(type='html', limit=1)
+        #import ipdb; ipdb.set_trace()
+        obj = val['objects'][0]
+        status = "Passed" if obj['success'] else "Failed"
+        date = parser.parse(obj['date'])
+        ret_val = "%s: http://readthedocs.org%s (%s)" % (status, obj['absolute_url'], timesince(date))
+        return ret_val
+    except Exception, e:
+        print "Status Error: %s" % e
+
+
+def Search(data):
+    cmd = '!%s ' % string.lower(Search.__name__)
+    term = data['message'].replace(cmd, '')
+    try:
+        val = api.file.search.get(q=term)
+        #import ipdb; ipdb.set_trace()
+        name = val['objects'][0]['project']['name']
+        url = val['objects'][0]['absolute_url']
+        text = val['objects'][0]['text']
+        text = re.sub('<[^<]+?>', '', text)
+        text = text[50:]
+        ret_val = "%s: http://readthedocs.org%s " % (name, url)
+        return ret_val
+    except Exception, e:
+        print "Search failed: %s" % e
 
 def Build(data):
     import inspect
@@ -91,6 +153,7 @@ def Build(data):
     except Exception, e:
         print "Error: %s" % e
         print "Moving on, yo."
+
 
 loco = locals().copy()
 shit_we_need = [loco[func] for func in loco if str(type(loco[func])) == "<type 'function'>" and loco[func].__name__[0].isupper()]
